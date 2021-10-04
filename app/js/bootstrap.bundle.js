932f12857c17e2a8ee5e643ef8de89eeb100593e
second1996 <vital10012@gmail.com>
2021-10-04 16:08:11 +0300

    git init

---
+++ app/js/bootstrap.bundle.js
@@ -0,0 +1,2 @@
+@@include('../../node_modules/bootstrap/js/dist/util.js') // Bootstrap Util lib
+@@include('../../node_modules/bootstrap/js/dist/modal.js') // Bootstrap Modal
---
+++ app/js/vendors.js
@@ -0,0 +1,2 @@
+@@include('../../node_modules/swiper/swiper-bundle.min.js') // Swiper slider
+@@include('../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js') // Fancybox gallery
