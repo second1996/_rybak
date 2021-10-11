042de0f3d34985b21362eb1b7194d54e7620f5b5
second1996 <vital10012@gmail.com>
2021-10-11 20:21:30 +0300

    Page: About

--- app/js/sliders.js
+++ app/js/sliders.js
@@ -1,5 +1,51 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function() {
+	/**
+	*-------------------------------------------------------------------------------------------------------------------------------------------
+	* Page About: Chronology slider
+	*-------------------------------------------------------------------------------------------------------------------------------------------
+	*/
+	const chronologySwiper = new Swiper('.a-chronology-slider', {
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+		},
+		pagination: {
+			el: '.a-chronology-pagination',
+			type: 'custom',
+			clickable: true,
+			renderCustom: function(swiper, current, total) {
+				let items = ''
+				swiper.slides.forEach((el, index) => {
+					const slideTitle = el.dataset.slideTitle
+
+					if ([current - 1] == index) {
+						items += `<span class="swiper-pagination-bullet swiper-pagination-bullet-active">${slideTitle}</span>`
+					} else {
+						items += `<span class="swiper-pagination-bullet">${slideTitle}</span>`
+					}
+				})
+				return items
+			},
+		},
+	})
 
 
-
+	/**
+	*-------------------------------------------------------------------------------------------------------------------------------------------
+	* Page About: Partners slider
+	*-------------------------------------------------------------------------------------------------------------------------------------------
+	*/
+	const partnersSwiper = new Swiper('.a-partners-slider .swiper', {
+		loop: true,
+		slidesPerView: 6,
+		navigation: {
+			prevEl: '.a-partners-navigation .swiper-button-prev',
+			nextEl: '.a-partners-navigation .swiper-button-next',
+		},
+		pagination: {
+			el: '.a-partners-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+	})
 })
