ba2c8b7e3d19ac9b48bbc2722355feb7d962262d
second1996 <vital10012@gmail.com>
2021-10-19 16:10:23 +0300

    Pages: COOP (Partners, Advertising, Provider, Realty), Factory. Refactored code

--- app/js/vendors.js
+++ app/js/vendors.js
@@ -1,2 +1,3 @@
 @@include('../../node_modules/swiper/swiper-bundle.min.js') // Swiper slider
 @@include('../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js') // Fancybox gallery
+@@include('../../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js') // jQuery Mask
