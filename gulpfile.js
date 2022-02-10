56969e8505c4449a93ee03afe73a6687659f1477
second1996 <vital10012@gmail.com>
2022-02-10 11:31:06 +0200

    Updated npm packages, gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,125 +2,130 @@
  * Variables & path
  */
 let preprocessor = 'sass', // Preprocessor (sass, scss, less, styl)
-		imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
-		sourceDir    = 'app', // Source directory path without «/» at the end
-		projectDir   = 'dist', // Dist directory path without «/» at the end
-		online       = true; // If «false» - Browsersync will work offline without internet connection
+  imageswatch = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
+  sourceDir = 'app', // Source directory path without «/» at the end
+  projectDir = 'dist', // Dist directory path without «/» at the end
+  online = true; // If «false» - Browsersync will work offline without internet connection
 
 /**
-* Project path
-*/
+ * Project path
+ */
 let path = {
-	build: {
-		html: projectDir + '/',
-		css: projectDir + '/css/',
-		js: projectDir + '/js/',
-		img: projectDir + '/images/',
-		fonts: projectDir + '/fonts/'
-	},
-	src: {
-		pug: [sourceDir + '/pug/pages/*.pug'],
-		css: sourceDir + '/' + preprocessor +'/**/*.' + preprocessor,
-		// js: [sourceDir + '/js/app.js', sourceDir + '/js/vendors.js'],
-		js: sourceDir + '/js/**/*.js',
-		img: sourceDir + '/images/**/*.{' + imageswatch + '}',
-		fonts: sourceDir + '/fonts/*.ttf'
-	},
-	watch: {
-		pug: sourceDir + '/pug/**/*.pug',
-		css: sourceDir + '/' + preprocessor +'/**/*.' + preprocessor,
-		js: sourceDir + '/js/**/*.js',
-		img: sourceDir + '/images/**/*.{' + imageswatch + '}',
-		svgIcons: sourceDir + '/svg-icons/*.svg'
-	},
-	clean: ['./' + projectDir + '/**', '!./' + projectDir, '!./' + projectDir + '/fonts']
-}
-
+  build: {
+    html: projectDir + '/',
+    css: projectDir + '/css/',
+    js: projectDir + '/js/',
+    img: projectDir + '/images/',
+    fonts: projectDir + '/fonts/',
+  },
+  src: {
+    pug: [sourceDir + '/pug/pages/*.pug'],
+    css: sourceDir + '/' + preprocessor + '/**/*.' + preprocessor,
+    // js: [sourceDir + '/js/app.js', sourceDir + '/js/vendors.js'],
+    js: sourceDir + '/js/**/*.js',
+    img: sourceDir + '/images/**/*.{' + imageswatch + '}',
+    fonts: sourceDir + '/fonts/*.ttf',
+  },
+  watch: {
+    pug: sourceDir + '/pug/**/*.pug',
+    css: sourceDir + '/' + preprocessor + '/**/*.' + preprocessor,
+    js: sourceDir + '/js/**/*.js',
+    img: sourceDir + '/images/**/*.{' + imageswatch + '}',
+    svgIcons: sourceDir + '/svg-icons/*.svg',
+  },
+  clean: ['./' + projectDir + '/**', '!./' + projectDir, '!./' + projectDir + '/fonts'],
+};
 
 /**
-* Logic
-*/
-const {src, dest}  = require('gulp');
-const gulp         = require('gulp');
-const browsersync  = require('browser-sync').create();
-const notify       = require('gulp-notify');
-const babel        = require('gulp-babel');
-const fileinclude  = require('gulp-file-include');
-const del          = require('del');
-const pug          = require('gulp-pug');
-const sass         = require('gulp-sass');
-const scss         = require('gulp-sass');
-const cleancss     = require('gulp-clean-css');
-const concat       = require('gulp-concat');
-const uglify       = require('gulp-uglify-es').default;
-const rename       = require('gulp-rename');
+ * Logic
+ */
+const { src, dest } = require('gulp');
+const gulp = require('gulp');
+const browsersync = require('browser-sync').create();
+const notify = require('gulp-notify');
+const babel = require('gulp-babel');
+const fileinclude = require('gulp-file-include');
+const del = require('del');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const scss = require('gulp-sass')(require('sass'));
+const cleancss = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
-const mediaqueries = require('gulp-group-css-media-queries')
-const imagemin     = require('gulp-imagemin');
-const newer        = require('gulp-newer');
-const svgsprite    = require('gulp-svg-sprite');
-const cheerio      = require('gulp-cheerio');
-const ttf2woff     = require('gulp-ttf2woff');
-const ttf2woff2    = require('gulp-ttf2woff2');
+const mediaqueries = require('gulp-group-css-media-queries');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const svgsprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
 
 /**
-* BrowserSync function
-*/
+ * BrowserSync function
+ */
 function browserSync() {
-	browsersync.init({
-		server: { baseDir: './' + projectDir + '/' },
-		notify: false,
-		online: online
-	})
+  browsersync.init({
+    server: { baseDir: './' + projectDir + '/' },
+    notify: false,
+    online: online,
+  });
 }
 
 /**
-* Clean dist folder
-*/
+ * Clean dist folder
+ */
 function clean() {
-	return del(path.clean)
+  return del(path.clean);
 }
 
 /**
-* HTML watch function
-*/
+ * HTML watch function
+ */
 function pug2html() {
-	return gulp.src(path.src.pug)
-		.pipe(pug({
-			pretty: '\t'
-		}))
-		.on('error', notify.onError(function (error) {
-			return {
-				title: 'Pug',
-				message: error.message
-			};
-		}))
-		.pipe(gulp.dest(path.build.html))
-		.pipe(browsersync.stream())
+  return gulp
+    .src(path.src.pug)
+    .pipe(
+      pug({
+        pretty: '\t',
+      }),
+    )
+    .on(
+      'error',
+      notify.onError(function (error) {
+        return {
+          title: 'Pug',
+          message: error.message,
+        };
+      }),
+    )
+    .pipe(gulp.dest(path.build.html))
+    .pipe(browsersync.stream());
 }
 
 /**
-* Styles watch functions
-*/
+ * Styles watch functions
+ */
 function styles() {
-	return src(path.src.css)
-		.pipe(eval(preprocessor)())
-		.pipe(mediaqueries())
-		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-		.pipe(cleancss( {level: { 1: { specialComments: 0 } }, format: 'beautify' }))
-		.pipe(dest(path.build.css))
-		.pipe(browsersync.stream())
+  return src(path.src.css)
+    .pipe(eval(preprocessor)())
+    .pipe(mediaqueries())
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+    .pipe(cleancss({ level: { 1: { specialComments: 0 } }, format: 'beautify' }))
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream());
 }
 
 /**
-* JavaScript watch function
-*/
+ * JavaScript watch function
+ */
 function scripts() {
-	return src(path.src.js)
-		.pipe(fileinclude())
-		.pipe(babel())
-		.pipe(dest(path.build.js))
-		.pipe(browsersync.stream())
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(babel())
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
 }
 // function scriptsMinify() {
 // 	return src(path.src.js)
@@ -132,81 +137,79 @@ function scripts() {
 // }
 
 /**
-* Images watch function
-*/
+ * Images watch function
+ */
 function images() {
-	return src(path.src.img)
-		.pipe(newer(path.build.img))
-		.pipe(
-			imagemin({
-				progressive: true,
-				svgoPlugins: [{ removeViewBox: false }],
-				interlaced: true,
-				optimizationLevel: 3, // 0 to 7
-			})
-		)
-		.pipe(dest(path.build.img))
+  return src(path.src.img)
+    .pipe(newer(path.build.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        interlaced: true,
+        optimizationLevel: 3, // 0 to 7
+      }),
+    )
+    .pipe(dest(path.build.img));
 }
 
 /**
-* SVG Sprite generate function
-*/
+ * SVG Sprite generate function
+ */
 function svgSprite() {
-	return src(path.watch.svgIcons)
-		.pipe(svgsprite({
-			mode: {
-				symbol: {
-					sprite: '../symbol-defs.svg'
-				}
-			},
-		}))
-		.pipe(cheerio({
-			run: function ($) {
-				$('[fill]').removeAttr('fill');
-				$('[stroke]').removeAttr('stroke');
-				$('[style]').removeAttr('style');
-				$('style').remove();
-			},
-			parserOptions: { xmlMode: true }
-		}))
-		.pipe(dest(path.build.img))
+  return src(path.watch.svgIcons)
+    .pipe(
+      svgsprite({
+        mode: {
+          symbol: {
+            sprite: '../symbol-defs.svg',
+          },
+        },
+      }),
+    )
+    .pipe(
+      cheerio({
+        run: function ($) {
+          $('[fill]').removeAttr('fill');
+          $('[stroke]').removeAttr('stroke');
+          $('[style]').removeAttr('style');
+          $('style').remove();
+        },
+        parserOptions: { xmlMode: true },
+      }),
+    )
+    .pipe(dest(path.build.img));
 }
 
 /**
-* Fonts convertation function (.ttf -> .woff, .woff2)
-*/
-gulp.task('fonts', function() {
-	src(path.src.fonts)
-		.pipe(ttf2woff())
-		.pipe(dest(path.build.fonts))
-	return src(path.src.fonts)
-		.pipe(ttf2woff2())
-		.pipe(dest(path.build.fonts))
-})
-
-
+ * Fonts convertation function (.ttf -> .woff, .woff2)
+ */
+gulp.task('fonts', function () {
+  src(path.src.fonts).pipe(ttf2woff()).pipe(dest(path.build.fonts));
+  return src(path.src.fonts).pipe(ttf2woff2()).pipe(dest(path.build.fonts));
+});
 
 /**
-* Watch files function
-*/
+ * Watch files function
+ */
 function watchFiles() {
-	gulp.watch([path.watch.pug], { usePolling: true }, pug2html)
-	gulp.watch([path.watch.css], { usePolling: true }, styles)
-	gulp.watch([path.watch.js], { usePolling: true }, scripts)
-	gulp.watch([path.watch.img], { usePolling: true }, images)
-	gulp.watch([path.watch.svgIcons], { usePolling: true }, svgSprite)
+  gulp.watch([path.watch.pug], { usePolling: true }, pug2html);
+  gulp.watch([path.watch.css], { usePolling: true }, styles);
+  gulp.watch([path.watch.js], { usePolling: true }, scripts);
+  gulp.watch([path.watch.img], { usePolling: true }, images);
+  gulp.watch([path.watch.svgIcons], { usePolling: true }, svgSprite);
 }
 
 const build = gulp.series(clean, gulp.parallel(scripts, styles, pug2html, images, svgSprite));
 const watch = gulp.parallel(build, watchFiles, browserSync);
 
-exports.svgSprite     = svgSprite;
-exports.images        = images;
-exports.clean         = clean;
-exports.scripts       = scripts;
+exports.svgSprite = svgSprite;
+exports.images = images;
+exports.clean = clean;
+exports.scripts = scripts;
 // exports.scriptsMinify = scriptsMinify;
-exports.styles        = styles;
-exports.pug2html      = pug2html;
-exports.build         = build;
-exports.watch         = watch;
-exports.default       = watch;
+exports.styles = styles;
+exports.pug2html = pug2html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
